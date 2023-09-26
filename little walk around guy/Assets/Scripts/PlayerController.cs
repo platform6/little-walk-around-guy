5391119a029cd883cda354f93443d03c59e5a570
platform6 <garrett.peter.conn@gmail.com>
2023-09-25 23:01:42 -0400

    added a background image - beans, too wet, space - nice

--- little walk around guy/Assets/Scripts/PlayerController.cs
+++ little walk around guy/Assets/Scripts/PlayerController.cs
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        pState = GetComponent<PlayerStateList>();
+     pState = GetComponent<PlayerStateList>();
      rb = GetComponent<Rigidbody2D>();
      anim = GetComponent<Animator>();
     }
